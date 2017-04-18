1e8e6a0eae02f01a5af1e2dbbdbd7de36bbdcfbd
kmdtmyk <winback_banderas@yahoo.co.jp>
2017-04-18 22:25:34 +0900

    コンポーネントを分離

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import App from './App'
+import Divide from './Divide'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', component: App },
+  {
+    path: '/', component: App,
+    children: [
+      { path: 'divide', component: Divide },
+    ],
+  },
 ]
 const router = new VueRouter({
   routes
