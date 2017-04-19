0bc78ed517cff36f57d932b6e5245ae7878ed755
kmdtmyk <winback_banderas@yahoo.co.jp>
2017-04-19 20:55:44 +0900

    出力先のディレクトリが勝手にできるようにした

--- src/ImageUtil.js
+++ src/ImageUtil.js
@@ -1,13 +1,12 @@
-const fs = global.require('fs')
+const fs = global.require('fs-extra')
 import path from 'path'
-
 
 export default class{
   
   static writeFileBase64(base64, filename){
     var base64Data = base64.replace(/^data:image\/png;base64,/, '')
     return new Promise((resolve) => {
-      fs.writeFile(filename, base64Data, 'base64', function(err){
+      fs.outputFile(filename, base64Data, 'base64', function(err){
         if(err){
           throw err
         }
